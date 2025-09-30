78174f028b4941d358be69545909a833daf68426
Kristine Braathen <kristinebraathen@Kristines-MacBook-Pro.local>
2025-09-30 21:21:30 +0200

    adds Strava-API endpoint

--- frontend/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-// Component exports
-export { default as Navbar } from './Navbar'
-export { default as Footer } from './Footer'
-export { default as NavLink } from './NavLink'
-export { default as VarslingButton } from './VarslingButton'
-export { default as RuntimeTitle } from './RuntimeTitle'
-export { default as StyledHeading } from './StyledHeading'
--- frontend/src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-// Page exports
-export { default as Home } from './Home'
-export { default as About } from './About'
-export { default as Contact } from './Contact'
-export { default as Report } from './Report'
